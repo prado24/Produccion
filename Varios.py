db6418576a863379eebff3f42bba89d76c06178e
prado <pradojimenezmaria@gmail.com>
2023-11-02 20:14:37 -0600

    Se agrego la clase de Varios

---
+++ Varios.py
@@ -0,0 +1,8 @@
+class Varios:
+    def LeerInt(self, entero):
+        return int(input(entero))
+    def LeerFloat(self, flotante):
+        return float(input(flotante))
+    def LeerString(self, texto):
+        return input(texto)
+    
